66a4bdf17eb80e926b112dc607f3e77c6d3ba891
이준규 <103472310+ljk0071@users.noreply.github.com>
2022-04-28 16:37:57 +0900

    1.0

---
+++ src/com/javaex/ex01/Ractangle.java
@@ -0,0 +1,31 @@
+package com.javaex.ex01;
+
+public class Ractangle extends Shape {
+	
+	private int width;
+	private int height;
+	
+	public Ractangle(String fillColor, int width, int height){
+		super(fillColor);
+		this.width=width;
+		this.height=height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void showInfo(){
+		System.out.println("[사각형]#면색:" +super.fillColor+ " "
+				+ " #가로:" + width 
+				+" #세로:" + height + "그렸습니다.");                                                                        
+	}
+	
+	public void draw() {
+		System.out.println("[사각형]#면색: "+fillColor+", #가로: "+width+", #세로: "+height+" 사각형을 그렸습니다.");
+	}
+		
+
+}
+
+
---
+++ src/com/javaex/ex03/SoundApp.java
@@ -0,0 +1,22 @@
+package com.javaex.ex03;
+
+public class SoundApp {
+
+    public static void main(String[] args) {
+        printSound( new Cat() );
+        printSound( new Dog() );
+        printSound( new Sparrow() );
+        printSound( new Duck() );
+    }
+
+    public static void printSound( Soundable soundable ) {
+        //구현
+    	while(true) {
+    		Soundable animal = soundable;
+    		System.out.println(animal.sound());
+    		break;
+    	}
+    }
+}
+
+
